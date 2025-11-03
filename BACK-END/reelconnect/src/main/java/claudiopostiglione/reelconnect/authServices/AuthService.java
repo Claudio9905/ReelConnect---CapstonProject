ddaccfcbbb451f5e7621396d42a694eacd7eb282
Claudio9905 <cloudpost10@gmail.com>
2025-11-03 17:05:39 +0100

    Add a method to check and create token for the login

--- BACK-END/reelconnect/src/main/java/claudiopostiglione/reelconnect/authServices/AuthService.java
+++ BACK-END/reelconnect/src/main/java/claudiopostiglione/reelconnect/authServices/AuthService.java
@@ -1,5 +1,6 @@
 package claudiopostiglione.reelconnect.authServices;
 
+import claudiopostiglione.reelconnect.payload.LoginDTO;
 import claudiopostiglione.reelconnect.security.JWTTools;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,11 @@ public class AuthService {
 
     @Autowired
     private JWTTools jwtTools;
+
+    public String checkAndCreateToken(LoginDTO bodyLogin){
+        //Controllo delle credenziali di accesso
+        
+    }
+
 
 }
