590cc81703f6c9dc6ac435a7b851e468fa6a685e
Claudio9905 <cloudpost10@gmail.com>
2025-11-03 10:25:56 +0100

    Initialization project with main settings and files

---
+++ BACK-END/reelconnect/src/main/java/claudiopostiglione/reelconnect/ReelconnectApplication.java
@@ -0,0 +1,13 @@
+package claudiopostiglione.reelconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReelconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReelconnectApplication.class, args);
+	}
+
+}
