3f540fa692e8ea8e2aedf4a695caaaa2e48b5882
Claudio9905 <cloudpost10@gmail.com>
2025-11-05 16:44:55 +0100

    Adds Navbar and intro Login component

--- FRONT-END/reelconnect/src/App.tsx
+++ FRONT-END/reelconnect/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Intro from "./components/Intro";
+import MyNavbar from "./components/MyNavbar";
 
 const App: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <MyNavbar />
+      <Intro />
+    </>
+  );
 };
 
 export default App;
---
+++ FRONT-END/reelconnect/src/components/Intro.tsx
@@ -0,0 +1,13 @@
+import Login from "./Login";
+import Register from "./Register";
+
+const Intro: React.FC = () => {
+  return (
+    <>
+      <Login />
+      <Register />
+    </>
+  );
+};
+
+export default Intro;
