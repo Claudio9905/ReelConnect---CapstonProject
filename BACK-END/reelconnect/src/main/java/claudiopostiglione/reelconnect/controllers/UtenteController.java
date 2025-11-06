e1f2225b540b3ad16c642a11a927a0aa5ed4b951
Claudio9905 <cloudpost10@gmail.com>
2025-11-06 16:11:12 +0100

    Added part about Authorization for Utenti and various method in UtenteController with path "/me" for current Utente

--- BACK-END/reelconnect/src/main/java/claudiopostiglione/reelconnect/controllers/UtenteController.java
+++ BACK-END/reelconnect/src/main/java/claudiopostiglione/reelconnect/controllers/UtenteController.java
@@ -7,6 +7,8 @@ import claudiopostiglione.reelconnect.services.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,12 +22,35 @@ public class UtenteController {
     @Autowired
     private UtenteService utenteService;
 
-    //Qui verranno posizionati gli endpoint per la perte degli utenti
+    // Qui verranno posizionati gli endpoint /me per l'utente corrente
+    // GET per il mio profilo
+    @GetMapping("/me")
+    public Utente getMyProfile(@AuthenticationPrincipal Utente currentUtente) {
+        return currentUtente;
+    }
 
+    //PUT per modifiche al mio profilo
+    @PutMapping("/me")
+    public Utente getMyProfileAndUpdate(@AuthenticationPrincipal Utente currentUtente, @RequestBody @Validated UtenteDTO bodyUtente, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            throw new ValidationException(validationResult.getFieldErrors().stream().map(fieldError -> fieldError.getDefaultMessage()).toList());
+        }
+        return this.utenteService.getUtenteByIdAndUpdate(currentUtente.getId(), bodyUtente);
+    }
+
+    //DELETE per eliminare il proprio profilo
+    @DeleteMapping("/me")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void getMyProfileAndDelete(@AuthenticationPrincipal Utente currentUtente) {
+        this.utenteService.getUtenteByIdAndDelete(currentUtente.getId());
+    }
+
+    //Qui verranno posizionati gli endpoint per la parte degli utenti
 
     // 1. GET per tutti gli utenti
     @GetMapping
     @ResponseStatus(HttpStatus.FOUND)
+    @PreAuthorize(("hasAnyAuthority('ADMIN', 'USER')"))
     public Page<Utente> findAllUtenti(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String sortBy) {
         return this.utenteService.getAllUtenteWithPagination(page, size, sortBy);
     }
@@ -33,6 +58,7 @@ public class UtenteController {
     // 2. POST per la creazione di un utente
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize(("hasAuthority('ADMIN')"))
     public Utente createUtente(@RequestBody @Validated UtenteDTO body, BindingResult validationResult) {
         if (validationResult.hasErrors()) {
             throw new ValidationException(validationResult.getFieldErrors().stream().map(fieldError -> fieldError.getDefaultMessage()).toList());
@@ -44,12 +70,14 @@ public class UtenteController {
     // 3. GET del singolo utente
     @GetMapping("/{idUtente}")
     @ResponseStatus(HttpStatus.FOUND)
+    @PreAuthorize(("hasAnyAuthority('ADMIN', 'USER')"))
     public Utente findUtenteById(@PathVariable UUID idUtente) {
         return this.utenteService.getUtenteById(idUtente);
     }
 
     // 4. PUT per la modifica dell'utente
     @PutMapping("/{idUtente}")
+    @PreAuthorize(("hasAuthority('ADMIN')"))
     public Utente findUtenteByIdAndUpdate(@PathVariable UUID idUtente, @RequestBody @Validated UtenteDTO body, BindingResult validationResult) {
         if (validationResult.hasErrors()) {
             throw new ValidationException(validationResult.getFieldErrors().stream().map(fieldError -> fieldError.getDefaultMessage()).toList());
@@ -60,6 +88,7 @@ public class UtenteController {
     // 5. DELETE per la cancellazione di un utente
     @DeleteMapping("/{idUtente}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize(("hasAuthority('ADMIN')"))
     public void findUtenteByIdAndDelete(@PathVariable UUID idUtente) {
         this.utenteService.getUtenteByIdAndDelete(idUtente);
     }
--- BACK-END/reelconnect/src/main/java/claudiopostiglione/reelconnect/security/JWTFilter.java
+++ BACK-END/reelconnect/src/main/java/claudiopostiglione/reelconnect/security/JWTFilter.java
@@ -1,22 +1,30 @@
 package claudiopostiglione.reelconnect.security;
 
+import claudiopostiglione.reelconnect.entities.utente.Utente;
+import claudiopostiglione.reelconnect.services.UtenteService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.UnavailableException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Component
 public class JWTFilter extends OncePerRequestFilter {
 
     @Autowired
     private JWTTools jwtTools;
+    @Autowired
+    private UtenteService utenteService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -33,8 +41,12 @@ public class JWTFilter extends OncePerRequestFilter {
         jwtTools.verifyToken(accessToken);
 //        ------[AUTHORIZATION]------------------------------------
 //        1. Ricerca dell'utente nel Database
+        UUID idUtente = jwtTools.extractIDFromToken(accessToken);
+        Utente utenteFound = this.utenteService.getUtenteById(idUtente);
 //        2. Associazione dell'utente al Security Context
+        Authentication authentication = new UsernamePasswordAuthenticationToken(utenteFound, null, utenteFound.getAuthorities());
 //        3. Aggiornamento del Security Context associando ad esso l'utente corrente e il suo ruolo
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 //        ---------------------------------------------------------
 
         filterChain.doFilter(request,response); // permetterà di continuare al prossimo filtro o direttamente al controller
