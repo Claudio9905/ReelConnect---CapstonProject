806c214795f3b58c95362de9ece0e31b53082971
Claudio9905 <cloudpost10@gmail.com>
2025-11-10 14:28:18 +0100

    Change port in localhost url

--- BACK-END/reelconnect/src/main/java/claudiopostiglione/reelconnect/security/SecurityConfig.java
+++ BACK-END/reelconnect/src/main/java/claudiopostiglione/reelconnect/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3005"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
 
