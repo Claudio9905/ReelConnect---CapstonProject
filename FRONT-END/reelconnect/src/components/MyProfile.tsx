e61576715df4ae847b0019f1f4ff15517bbac7f0
Claudio9905 <cloudpost10@gmail.com>
2025-11-29 17:41:51 +0100

    Modify CSS file

--- FRONT-END/reelconnect/src/components/MyProfile.tsx
+++ FRONT-END/reelconnect/src/components/MyProfile.tsx
@@ -1,5 +1,4 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
-import NavSettings from "./NavSettings";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../redux/store";
@@ -149,68 +148,224 @@ const MyProfile: React.FC = () => {
       {/* Versione tablet */}
       <Container
         id="container-my-profile"
-        className="d-none d-md-flex d-lg-none"
+        className="d-none d-md-flex d-lg-none flex-column"
       >
         <Row id="row-1" className="d-flex flex-column align-items-center">
-          <Col xs={12} id="col-profile-1-md" className="d-flex flex-column">
+          <Col xs={12} id="col-profile-1-sm" className="d-flex flex-column">
             {/* Qui ci sarà la l'immagine di copertina, l'immagine di profilo */}
-            <div className="banner-div-md">
-              <img src="" alt="immagine di copertina" />
-            </div>
-            <div className=" img-profile-div-md">
-              <img src="" alt="immagine di profilo" />
+            <div className="banner-div-sm">
+              <img
+                src={profile.bannerUrl}
+                alt="immagine di copertina"
+                className="img-fluid "
+                id="banner-profile"
+              />
+              <Button onClick={handleShowModalBanner} id="button-edit-banner">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="bi bi-pencil-square"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                  />
+                </svg>
+              </Button>
+              {showModalBanner && (
+                <ModaleEditBannerProfile
+                  onClose={handleCloseModalBanner}
+                  onShow={showModalBanner}
+                />
+              )}
             </div>
           </Col>
-          <Col xs={12} id="col-profile-2-md" className="d-flex flex-column">
+          <Col
+            xs={12}
+            id="col-profile-2-sm"
+            className="d-flex flex-column align-items-center"
+          >
             {/* le informazione dell'utente */}
-            <h1>Nome profilo - username</h1>
-            <h3>Nome completo - nome/cognome</h3>
-            <h3>Info:</h3>
-            <p>
-              | E-mail: <br />
-              | Età: <br />
-              | Data di nascita: <br />
-              | Sesso: <br />
-              ...
-            </p>
-          </Col>
-        </Row>
-        <Row id="row-2" className="d-flex flex-column align-items-center">
-          <MyPost />
-        </Row>
-      </Container>
-
-      {/* // Versione Desktop */}
-      <Container id="container-my-profile" className="d-none d-lg-flex">
-        <Row id="row-1" className="d-flex flex-column align-items-center">
-          <Col xs={12} id="col-profile-1-lg" className="d-flex flex-column">
-            {/* Qui ci sarà la l'immagine di copertina, l'immagine di profilo */}
-            <div className="banner-div-lg">
-              <img src="" alt="immagine di copertina" />
+            <div className="div-name-profile-sm">
+              <h1 className="fs-3" id="name-profile-sm">
+                {profile.nome} {profile.cognome}
+              </h1>
+              <h3 className="fs-6" id="username-profile-sm">
+                {profile.username}
+              </h3>
+              <div className=" img-profile-div-sm">
+                <img
+                  src={profile.avatarUrl}
+                  alt="immagine di profilo"
+                  className="img-fluid object-fit-cover"
+                  id="avatar-profile"
+                />
+                <Button onClick={handleShowModalAvatar} id="button-edit-avatar">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-pencil-square"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                    />
+                  </svg>
+                </Button>
+                {showModalAvatar && (
+                  <ModaleEditAvatarProfile
+                    onClose={handleCloseModalAvatar}
+                    onShow={showModalAvatar}
+                  />
+                )}
+              </div>
+              <div className="div-fan-follow">
+                <Button id="button-number-fan">N. FAN</Button>
+              </div>
             </div>
-            <div className=" img-profile-div-lg">
-              <img src="" alt="immagine di profilo" />
+            <div className="d-flex flex-column justify-content-around p-2">
+              <p>
+                | E-mail: {profile.email} <br />| Età: {profile.eta} <br />|
+                Data di nascita: {profile.dataDiNascita}
+                <br />| Sesso: {profile.sesso} <br />
+              </p>
+              <Button className="button-edit-info" onClick={handleShow}>
+                EDIT
+              </Button>
+              {showModal && (
+                <ModaleEditProfile onClose={handleClose} onShow={showModal} />
+              )}
             </div>
-          </Col>
-          <Col xs={12} id="col-profile-2-lg" className="d-flex flex-column">
-            {/* le informazione dell'utente */}
-            <h1>Nome profilo - username</h1>
-            <h3>Nome completo - nome/cognome</h3>
-            <h3>Info:</h3>
-            <p>
-              | E-mail: <br />
-              | Età: <br />
-              | Data di nascita: <br />
-              | Sesso: <br />
-              ...
-            </p>
+            <span className="span-divided-sm"></span>
+            {/* Componente dei film/serie Tv visti */}
+            <div className="catalogo-filmSerieTv">
+              <h3> Film/SerieTv visti </h3>
+            </div>
           </Col>
         </Row>
         <Row id="row-2" className="d-flex flex-column align-items-center">
           {/* Qui ci sarà la sezione dei post */}
           <MyPost />
         </Row>
-        <NavSettings />
+      </Container>
+      {/* Versione desktop */}
+      <Container
+        id="container-my-profile"
+        className="d-none d-lg-flex flex-column"
+      >
+        <Row id="row-1" className="d-flex flex-column align-items-center">
+          <Col xs={12} id="col-profile-1-sm" className="d-flex flex-row">
+            {/* Qui ci sarà la l'immagine di copertina, l'immagine di profilo */}
+            <div className="banner-div-sm">
+              <img
+                src={profile.bannerUrl}
+                alt="immagine di copertina"
+                className="img-fluid "
+                id="banner-profile"
+              />
+              <Button onClick={handleShowModalBanner} id="button-edit-banner">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="bi bi-pencil-square"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                  />
+                </svg>
+              </Button>
+              {showModalBanner && (
+                <ModaleEditBannerProfile
+                  onClose={handleCloseModalBanner}
+                  onShow={showModalBanner}
+                />
+              )}
+            </div>
+          </Col>
+          <Col
+            xs={12}
+            id="col-profile-2-sm"
+            className="d-flex flex-row align-items-center"
+          >
+            {/* le informazione dell'utente */}
+            <div className="div-name-profile-sm">
+              <h1 className="fs-3" id="name-profile-sm">
+                {profile.nome} {profile.cognome}
+              </h1>
+              <h3 className="fs-6" id="username-profile-sm">
+                {profile.username}
+              </h3>
+              <div className=" img-profile-div-sm">
+                <img
+                  src={profile.avatarUrl}
+                  alt="immagine di profilo"
+                  className="img-fluid object-fit-cover"
+                  id="avatar-profile"
+                />
+                <Button onClick={handleShowModalAvatar} id="button-edit-avatar">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-pencil-square"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                    />
+                  </svg>
+                </Button>
+                {showModalAvatar && (
+                  <ModaleEditAvatarProfile
+                    onClose={handleCloseModalAvatar}
+                    onShow={showModalAvatar}
+                  />
+                )}
+              </div>
+              <div className="div-fan-follow">
+                <Button id="button-number-fan">N. FAN</Button>
+              </div>
+            </div>
+            <div className="d-flex flex-column justify-content-around p-2">
+              <p>
+                | E-mail: {profile.email} <br />| Età: {profile.eta} <br />|
+                Data di nascita: {profile.dataDiNascita}
+                <br />| Sesso: {profile.sesso} <br />
+              </p>
+              <Button className="button-edit-info" onClick={handleShow}>
+                EDIT
+              </Button>
+              {showModal && (
+                <ModaleEditProfile onClose={handleClose} onShow={showModal} />
+              )}
+            </div>
+            <span className="span-divided-sm"></span>
+            {/* Componente dei film/serie Tv visti */}
+            <div className="catalogo-filmSerieTv">
+              <h3> Film/SerieTv visti </h3>
+            </div>
+          </Col>
+        </Row>
+        <Row id="row-2" className="d-flex flex-column align-items-center">
+          {/* Qui ci sarà la sezione dei post */}
+          <MyPost />
+        </Row>
       </Container>
     </>
   );
--- FRONT-END/reelconnect/src/components/NavSettings.tsx
+++ FRONT-END/reelconnect/src/components/NavSettings.tsx
@@ -23,7 +23,7 @@ const NavSettings: React.FC = () => {
       {/* Versione mobile */}
       <Container
         id="container-NavSettings"
-        className={` d-sm-flex d-md-none d-lg-none flex-column container-nav-settings
+        className={` d-sm-flex d-none flex-column container-nav-settings
           ${location.pathname === "/" ? "d-none" : ""}`}
       >
         <Row>
@@ -55,7 +55,7 @@ const NavSettings: React.FC = () => {
               <img
                 src={iconaCineverse}
                 alt="icona pagina cineverse"
-                className="img-fluid w-75"
+                className="img-fluid icona-cineverse-sm"
               />
             </Button>
             <Button
@@ -113,44 +113,180 @@ const NavSettings: React.FC = () => {
       {/* Versione tablet */}
       <Container
         id="container-NavSettings"
-        className="d-none d-md-flex d-lg-none"
+        className={` d-none d-md-flex d-lg-none flex-column container-nav-settings
+          ${location.pathname === "/" ? "d-none" : ""}`}
       >
         <Row>
           <Col xs={12} className="d-flex justify-content-around">
-            <Button>
+            <Button id="button-search-user">
               {/* Per la ricerca degli utenti dove si aprirà un modale apposito */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                className="bi bi-person-bounding-box"
+                viewBox="0 0 16 16"
+              >
+                <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5" />
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+              </svg>
             </Button>
             <Button
+              id="button-navigate-cineverse"
               onClick={() => {
                 navigate(`/cineverse`);
               }}
+              className={`w-25 ${
+                location.pathname === "/cineverse" ? "d-none" : ""
+              }`}
             >
               {/* Per accedere al Cineverse */}
+              <img
+                src={iconaCineverse}
+                alt="icona pagina cineverse"
+                className="img-fluid w-50"
+              />
             </Button>
-            <Button>{/* Per la creazione di un post */}</Button>
-            <Button>
+            <Button
+              id="button-navigate-myprofile"
+              onClick={() => {
+                navigate("/myprofile");
+              }}
+              className={`${
+                location.pathname === "/myprofile" ? "d-none" : ""
+              } `}
+            >
+              {/* Per accedere al proprio profilo */}
+              <img
+                src={myProfile.avatarUrl}
+                alt="icona-profilo"
+                className="img-fluid w-25 rounded-5 object-fit-contain "
+              />
+            </Button>
+            <Button id="button-create-post" onClick={handleshowModale}>
+              {/* Per la creazione di un post */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                className="bi bi-file-post"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4 3.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5z" />
+                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1" />
+              </svg>
+            </Button>
+            {showCreatePost && (
+              <ModaleCreatePost
+                onClose={handleNoshowModale}
+                onShow={showCreatePost}
+              />
+            )}
+            <Button id="button-lista-film-serieTv">
               {/* Per accedere alla lista dei propri film/serieTv viste */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                className="bi bi-film"
+                viewBox="0 0 16 16"
+              >
+                <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z" />
+              </svg>
             </Button>
           </Col>
         </Row>
       </Container>
-      {/* Versione Desktop*/}
-      <Container id="container-NavSettings" className="d-none d-lg-flex">
+      {/* Versione desktop */}
+      <Container
+        id="container-NavSettings"
+        className={`d-lg-flex flex-column container-nav-settings
+          ${location.pathname === "/" ? "d-none" : ""}`}
+      >
         <Row>
           <Col xs={12} className="d-flex justify-content-around">
-            <Button>
+            <Button id="button-search-user">
               {/* Per la ricerca degli utenti dove si aprirà un modale apposito */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                className="bi bi-person-bounding-box"
+                viewBox="0 0 16 16"
+              >
+                <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5" />
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+              </svg>
             </Button>
             <Button
+              id="button-navigate-cineverse"
               onClick={() => {
                 navigate(`/cineverse`);
               }}
+              className={`w-25 ${
+                location.pathname === "/cineverse" ? "d-none" : ""
+              }`}
             >
               {/* Per accedere al Cineverse */}
+              <img
+                src={iconaCineverse}
+                alt="icona pagina cineverse"
+                className="img-fluid"
+                id="icona-cineverse-lg"
+              />
             </Button>
-            <Button>{/* Per la creazione di un post */}</Button>
-            <Button>
+            <Button
+              id="button-navigate-myprofile"
+              onClick={() => {
+                navigate("/myprofile");
+              }}
+              className={`${
+                location.pathname === "/myprofile" ? "d-none" : ""
+              } `}
+            >
+              {/* Per accedere al proprio profilo */}
+              <img
+                src={myProfile.avatarUrl}
+                alt="icona-profilo"
+                className="img-fluid w-25 rounded-5 object-fit-contain "
+              />
+            </Button>
+            <Button id="button-create-post" onClick={handleshowModale}>
+              {/* Per la creazione di un post */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                className="bi bi-file-post"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4 3.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5z" />
+                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1" />
+              </svg>
+            </Button>
+            {showCreatePost && (
+              <ModaleCreatePost
+                onClose={handleNoshowModale}
+                onShow={showCreatePost}
+              />
+            )}
+            <Button id="button-lista-film-serieTv">
               {/* Per accedere alla lista dei propri film/serieTv viste */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                className="bi bi-film"
+                viewBox="0 0 16 16"
+              >
+                <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z" />
+              </svg>
             </Button>
           </Col>
         </Row>
