4ea63a378f196507d4d747c6ebb70bc58b615cb0
Claudio9905 <cloudpost10@gmail.com>
2025-12-02 01:01:50 +0100

    Update CSS files

--- FRONT-END/reelconnect/src/components/Service.tsx
+++ FRONT-END/reelconnect/src/components/Service.tsx
@@ -40,7 +40,7 @@ const Service: React.FC = () => {
     <>
       {/* verione mobile */}
       {myProfile.role !== "ADMIN" && (
-        <Container className="d-sm-flex d-md-none d-lg-none h-100">
+        <Container className="d-md-none d-lg-none">
           <Row className="p-3 d-flex">
             <Col className="col-1-serviceLoginFail">
               <div className="text-center p-1">
@@ -59,7 +59,7 @@ const Service: React.FC = () => {
       {myProfile.role === "ADMIN" && (
         <Container className="d-sm-flex d-md-none d-lg-none flex-column">
           <div className="d-flex justify-content-around mb-3 mt-4">
-            <Button className="button-with-icon " type="submit">
+            <Button className="button-with-icon  " type="submit">
               <svg
                 className="icon"
                 id="Play"
@@ -422,6 +422,217 @@ const Service: React.FC = () => {
                   <Card.Text>Anno di uscita</Card.Text>
                   <div className="d-flex justify-content-around gap-3">
                     <Button className="button-with-icon" type="submit">
+                      <svg
+                        className="icon"
+                        id="Play"
+                        viewBox="0 0 48 48"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          className="color000000 svgShape"
+                          fill="#ffffff"
+                          d="M12 39c-.549 0-1.095-.15-1.578-.447A3.008 3.008 0 0 1 9 36V12c0-1.041.54-2.007 1.422-2.553a3.014 3.014 0 0 1 2.919-.132l24 12a3.003 3.003 0 0 1 0 5.37l-24 12c-.42.21-.885.315-1.341.315z"
+                        ></path>
+                      </svg>
+                      <span className="text">Edit</span>
+                    </Button>
+                    <Button className="button-with-icon" type="submit">
+                      <svg
+                        className="icon"
+                        id="Play"
+                        viewBox="0 0 48 48"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          className="color000000 svgShape"
+                          fill="#ffffff"
+                          d="M12 39c-.549 0-1.095-.15-1.578-.447A3.008 3.008 0 0 1 9 36V12c0-1.041.54-2.007 1.422-2.553a3.014 3.014 0 0 1 2.919-.132l24 12a3.003 3.003 0 0 1 0 5.37l-24 12c-.42.21-.885.315-1.341.315z"
+                        ></path>
+                      </svg>
+                      <span className="text">Delete</span>
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+            {/* );
+              })} */}
+          </Row>
+        </Container>
+      )}
+      {/* versione desktop */}
+      {myProfile.role !== "ADMIN" && (
+        <Container className="d-none d-lg-flex flex-column ">
+          <Row className="p-3 d-flex">
+            <Col className="col-1-serviceLoginFail">
+              <div className="text-center p-1">
+                <h3 className="fs-6 ">Questo account non ha un ruolo admin</h3>
+                <a
+                  href="/serviceLogin"
+                  className=" text-decoration-none text-light"
+                >
+                  Ritorna al login
+                </a>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      )}
+      {myProfile.role === "ADMIN" && (
+        <Container className="d-none d-lg-flex flex-column">
+          <div className="d-flex justify-content-around mb-3 mt-4">
+            <Button className="button-with-icon " type="submit">
+              <svg
+                className="icon"
+                id="Play"
+                viewBox="0 0 48 48"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  className="color000000 svgShape"
+                  fill="#ffffff"
+                  d="M12 39c-.549 0-1.095-.15-1.578-.447A3.008 3.008 0 0 1 9 36V12c0-1.041.54-2.007 1.422-2.553a3.014 3.014 0 0 1 2.919-.132l24 12a3.003 3.003 0 0 1 0 5.37l-24 12c-.42.21-.885.315-1.341.315z"
+                ></path>
+              </svg>
+              <span className="text p-1"> + Film</span>
+            </Button>
+            <Button className="button-with-icon " type="submit">
+              <svg
+                className="icon"
+                id="Play"
+                viewBox="0 0 48 48"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  className="color000000 svgShape"
+                  fill="#ffffff"
+                  d="M12 39c-.549 0-1.095-.15-1.578-.447A3.008 3.008 0 0 1 9 36V12c0-1.041.54-2.007 1.422-2.553a3.014 3.014 0 0 1 2.919-.132l24 12a3.003 3.003 0 0 1 0 5.37l-24 12c-.42.21-.885.315-1.341.315z"
+                ></path>
+              </svg>
+              <span className="text"> + Serie Tv</span>
+            </Button>
+          </div>
+          <h3 className="title-catalogo">Film presenti nel catalogo :</h3>
+          <Row id="row-serviceFilm" className="p-5 d-flex  ">
+            {/* {Array.isArray(movies) &&
+              movies.map((film) => {
+                return ( */}
+            <Col xs={12} md={6} lg={4} className="col1-film-lg">
+              <Card className="card-film-serieTv-lg">
+                <Card.Img
+                  variant="top"
+                  src={bannerFilm}
+                  className="img-fluid cover-image"
+                />
+                <Card.Body className="d-flex flex-column align-items-center justify-content-center body-card-catalogo ">
+                  <Card.Title className="title-card-catalogo">
+                    Titolo Film
+                  </Card.Title>
+                  <Card.Text className="d-flex ">Anno di uscita</Card.Text>
+                  <div className="d-flex justify-content-between gap-3">
+                    <Button className="button-with-icon w-50" type="submit">
+                      <svg
+                        className="icon"
+                        id="Play"
+                        viewBox="0 0 48 48"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          className="color000000 svgShape"
+                          fill="#ffffff"
+                          d="M12 39c-.549 0-1.095-.15-1.578-.447A3.008 3.008 0 0 1 9 36V12c0-1.041.54-2.007 1.422-2.553a3.014 3.014 0 0 1 2.919-.132l24 12a3.003 3.003 0 0 1 0 5.37l-24 12c-.42.21-.885.315-1.341.315z"
+                        ></path>
+                      </svg>
+                      <span className="text">Edit</span>
+                    </Button>
+                    <Button className="button-with-icon " type="submit">
+                      <svg
+                        className="icon"
+                        id="Play"
+                        viewBox="0 0 48 48"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          className="color000000 svgShape"
+                          fill="#ffffff"
+                          d="M12 39c-.549 0-1.095-.15-1.578-.447A3.008 3.008 0 0 1 9 36V12c0-1.041.54-2.007 1.422-2.553a3.014 3.014 0 0 1 2.919-.132l24 12a3.003 3.003 0 0 1 0 5.37l-24 12c-.42.21-.885.315-1.341.315z"
+                        ></path>
+                      </svg>
+                      <span className="text">Delete</span>
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={12} md={6} lg={4} className="col1-film-lg">
+              <Card className="card-film-serieTv-lg">
+                <Card.Img
+                  variant="top"
+                  src={bannerFilm}
+                  className="img-fluid cover-image"
+                />
+                <Card.Body className="d-flex flex-column align-items-center justify-content-center body-card-catalogo ">
+                  <Card.Title className="title-card-catalogo">
+                    Titolo Film
+                  </Card.Title>
+                  <Card.Text className="d-flex ">Anno di uscita</Card.Text>
+                  <div className="d-flex justify-content-between gap-3">
+                    <Button className="button-with-icon w-50 " type="submit">
+                      <svg
+                        className="icon"
+                        id="Play"
+                        viewBox="0 0 48 48"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          className="color000000 svgShape"
+                          fill="#ffffff"
+                          d="M12 39c-.549 0-1.095-.15-1.578-.447A3.008 3.008 0 0 1 9 36V12c0-1.041.54-2.007 1.422-2.553a3.014 3.014 0 0 1 2.919-.132l24 12a3.003 3.003 0 0 1 0 5.37l-24 12c-.42.21-.885.315-1.341.315z"
+                        ></path>
+                      </svg>
+                      <span className="text">Edit</span>
+                    </Button>
+                    <Button className="button-with-icon" type="submit">
+                      <svg
+                        className="icon"
+                        id="Play"
+                        viewBox="0 0 48 48"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          className="color000000 svgShape"
+                          fill="#ffffff"
+                          d="M12 39c-.549 0-1.095-.15-1.578-.447A3.008 3.008 0 0 1 9 36V12c0-1.041.54-2.007 1.422-2.553a3.014 3.014 0 0 1 2.919-.132l24 12a3.003 3.003 0 0 1 0 5.37l-24 12c-.42.21-.885.315-1.341.315z"
+                        ></path>
+                      </svg>
+                      <span className="text">Delete</span>
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+            {/* ); */}
+            {/* })} */}
+          </Row>
+          <h3 className=" title-catalogo"> Serie Tv presenti nel catalogo :</h3>
+          <Row id="row-serviceSerieTv" className="p-5 d-flex ">
+            {/* {Array.isArray(seriesTV) &&
+              seriesTV.map((seriesTV) => {
+                return ( */}
+            <Col xs={12} md={6} lg={4} className="col2-serieTv-lg">
+              <Card className="card-film-serieTv-lg">
+                <Card.Img
+                  variant="top"
+                  src={bannerFilm}
+                  className="img-fluid cover-image"
+                />
+                <Card.Body className="d-flex flex-column align-items-center justify-content-center body-card-catalogo ">
+                  <Card.Title className="title-card-catalogo">
+                    Titolo SerieTv
+                  </Card.Title>
+                  <Card.Text>Anno di uscita</Card.Text>
+                  <div className="d-flex justify-content-around gap-3">
+                    <Button className="button-with-icon w-50" type="submit">
                       <svg
                         className="icon"
                         id="Play"
