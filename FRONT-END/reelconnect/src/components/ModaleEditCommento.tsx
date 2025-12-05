42af837c06dfbc7e2e7b54a478fa36eaa40c8fe8
Claudio9905 <cloudpost10@gmail.com>
2025-12-05 16:30:15 +0100

    Added Edit Post component and Edit Commento component

---
+++ FRONT-END/reelconnect/src/components/ModaleEditCommento.tsx
@@ -0,0 +1,5 @@
+const ModaleEditCommento: React.FC = () => {
+  return <></>;
+};
+
+export default ModaleEditCommento;
---
+++ FRONT-END/reelconnect/src/components/ModaleEditPost.tsx
@@ -0,0 +1,10 @@
+type propsTypes = {
+  onShow: boolean;
+  onCLose: () => void;
+};
+
+const ModaleEditPost: React.FC<propsTypes> = ({ onShow, onCLose }) => {
+  return <></>;
+};
+
+export default ModaleEditPost;
